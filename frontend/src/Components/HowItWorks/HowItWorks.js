59be0ad6694fde42e00edb5e24efd4b7bcf5408b
Yash Gupta <hpsyash@gmail.com>
2021-01-21 19:52:41 +0530

    updated how it works

--- frontend/src/Components/HowItWorks/HowItWorks.js
+++ frontend/src/Components/HowItWorks/HowItWorks.js
@@ -1,46 +1,33 @@
 import React, { useState } from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron'
+import Card from 'react-bootstrap/Card'
+import './HowItWorks.css';
 
 
 const Workingg = () => {
 
     return (
+
+        
+
+
         <Jumbotron>
             <h1>How It Works?</h1>
             <p> </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam volutpat,
-                quam eu gravida suscipit, lacus lorem bibendum libero, accumsan pulvinar
-                ante mi ac tortor. Integer cursus nunc et maximus bibendum. Aenean diam
-                justo, fermentum id pretium non, tempor non felis. Maecenas mattis
-                elementum sollicitudin. Praesent laoreet tellus nunc, a tempus arcu
-                vulputate consequat. Mauris sollicitudin odio nec dolor imperdiet
-                molestie. Proin efficitur at ligula quis auctor. Phasellus
-                condimentum eleifend sem. Vivamus vel convallis sem. In in erat eros.
-                Nam pellentesque diam sed felis aliquam, sed venenatis dui condimentum.
-                Mauris placerat finibus neque vel tincidunt. Etiam malesuada tempor velit,
-                a vestibulum ante blandit non.
-
-            </p>
-            <p> </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam volutpat,
-                quam eu gravida suscipit, lacus lorem bibendum libero, accumsan pulvinar
-                ante mi ac tortor. Integer cursus nunc et maximus bibendum. Aenean diam
-                justo, fermentum id pretium non, tempor non felis. Maecenas mattis
-                elementum sollicitudin. Praesent laoreet tellus nunc, a tempus arcu
-                vulputate consequat. Mauris sollicitudin odio nec dolor imperdiet
-                molestie. Proin efficitur at ligula quis auctor. Phasellus
-                condimentum eleifend sem. Vivamus vel convallis sem. In in erat eros.
-                Nam pellentesque diam sed felis aliquam, sed venenatis dui condimentum.
-                Mauris placerat finibus neque vel tincidunt. Etiam malesuada tempor velit,
-                a vestibulum ante blandit non.
-
-            </p>
+            <div className = 'left-card'>
+            <Card body>1. You Login/Signup</Card>
+            </div>
+            <div className = 'right-card'>
+            <Card body>2. We authenticate</Card>
+            </div>
+            <div className = 'left-card'>
+            <Card body>3. You submit a complaint</Card>
+            </div>
+            <div className = 'right-card'>
+            <Card body>4. We Resolve 😎😎 </Card>
+            </div>
            
-            <p>
-
-            </p>
+            
         </Jumbotron>
     );
 }
