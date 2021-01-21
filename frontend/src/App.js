4d260c8e33a98df616edbb3a266716e3112d8390
Yash Gupta <hpsyash@gmail.com>
2021-01-21 19:15:38 +0530

    added how it works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
+import HowItWorks from './Components/HowItWorks/HowItWorks';
 
 function App() {
   return (
     <div>
-      App
+      <HowItWorks />
     </div>
   );
 }
---
+++ frontend/src/Components/HowItWorks/HowItWorks.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+
+
+const Workingg = () => {
+
+    return (
+        <Jumbotron>
+            <h1>How It Works?</h1>
+            <p> </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam volutpat,
+                quam eu gravida suscipit, lacus lorem bibendum libero, accumsan pulvinar
+                ante mi ac tortor. Integer cursus nunc et maximus bibendum. Aenean diam
+                justo, fermentum id pretium non, tempor non felis. Maecenas mattis
+                elementum sollicitudin. Praesent laoreet tellus nunc, a tempus arcu
+                vulputate consequat. Mauris sollicitudin odio nec dolor imperdiet
+                molestie. Proin efficitur at ligula quis auctor. Phasellus
+                condimentum eleifend sem. Vivamus vel convallis sem. In in erat eros.
+                Nam pellentesque diam sed felis aliquam, sed venenatis dui condimentum.
+                Mauris placerat finibus neque vel tincidunt. Etiam malesuada tempor velit,
+                a vestibulum ante blandit non.
+
+            </p>
+            <p> </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam volutpat,
+                quam eu gravida suscipit, lacus lorem bibendum libero, accumsan pulvinar
+                ante mi ac tortor. Integer cursus nunc et maximus bibendum. Aenean diam
+                justo, fermentum id pretium non, tempor non felis. Maecenas mattis
+                elementum sollicitudin. Praesent laoreet tellus nunc, a tempus arcu
+                vulputate consequat. Mauris sollicitudin odio nec dolor imperdiet
+                molestie. Proin efficitur at ligula quis auctor. Phasellus
+                condimentum eleifend sem. Vivamus vel convallis sem. In in erat eros.
+                Nam pellentesque diam sed felis aliquam, sed venenatis dui condimentum.
+                Mauris placerat finibus neque vel tincidunt. Etiam malesuada tempor velit,
+                a vestibulum ante blandit non.
+
+            </p>
+            <p> </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam volutpat,
+                quam eu gravida suscipit, lacus lorem bibendum libero, accumsan pulvinar
+                ante mi ac tortor. Integer cursus nunc et maximus bibendum. Aenean diam
+                justo, fermentum id pretium non, tempor non felis. Maecenas mattis
+                elementum sollicitudin. Praesent laoreet tellus nunc, a tempus arcu
+                vulputate consequat. Mauris sollicitudin odio nec dolor imperdiet
+                molestie. Proin efficitur at ligula quis auctor. Phasellus
+                condimentum eleifend sem. Vivamus vel convallis sem. In in erat eros.
+                Nam pellentesque diam sed felis aliquam, sed venenatis dui condimentum.
+                Mauris placerat finibus neque vel tincidunt. Etiam malesuada tempor velit,
+                a vestibulum ante blandit non.
+
+            </p>
+            <p> </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam volutpat,
+                quam eu gravida suscipit, lacus lorem bibendum libero, accumsan pulvinar
+                ante mi ac tortor. Integer cursus nunc et maximus bibendum. Aenean diam
+                justo, fermentum id pretium non, tempor non felis. Maecenas mattis
+                elementum sollicitudin. Praesent laoreet tellus nunc, a tempus arcu
+                vulputate consequat. Mauris sollicitudin odio nec dolor imperdiet
+                molestie. Proin efficitur at ligula quis auctor. Phasellus
+                condimentum eleifend sem. Vivamus vel convallis sem. In in erat eros.
+                Nam pellentesque diam sed felis aliquam, sed venenatis dui condimentum.
+                Mauris placerat finibus neque vel tincidunt. Etiam malesuada tempor velit,
+                a vestibulum ante blandit non.
+
+            </p>
+            <p>
+
+            </p>
+        </Jumbotron>
+    );
+}
+
+export default Workingg;
