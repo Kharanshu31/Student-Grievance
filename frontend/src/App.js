00fad92c33cbe5cb8042e658ee574ba4d0d7f66a
Yash Gupta <hpsyash@gmail.com>
2021-01-21 23:47:27 +0530

    resolved rahul conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import React from 'react';
-import HowItWorks from './Components/HowItWorks/HowItWorks';
-import ContactUs from './Components/ContactUs/ContactUs'
-=======
-import Login from "./Components/Login";
-import Register from "./Components/Register";
->>>>>>> 68226eea1cc956e5acef4e4a3cb8d25fcf79933c
-=======
-
 import Login from "./Components/Login";
 import Register from "./Components/Register";
 import React from 'react';
 import HowItWorks from './Components/HowItWorks/HowItWorks';
 import ContactUs from './Components/ContactUs/ContactUs'
 
->>>>>>> 8a03385796de81a2c664073689380e2922ec6cc0
 
 function App() {
   return (
