3cdcdfa5fec75c9d768e98656f274c5f0f9af5cc
Kharanshu31 <dewankharanshu@gmail.com>
2021-01-22 22:26:33 +0530

    Integrate with backend

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,8 +1,55 @@
 import React, { Component } from "react";
 import pic from "../images/8576.jpg";
 import "../css/login.css";
+import axios from "axios";
+import swal from "sweetalert";
 
 class Login extends Component {
+
+  state={
+    email:"",
+    password:""
+  }
+
+  handleChangeEmail=(e)=>{
+    const n=e.target.value;
+    this.setState({email:n});
+  }
+
+  handleChangePassword=(e)=>{
+    const n=e.target.value;
+    this.setState({password:n});
+  }
+
+  loginUser=(e)=>{
+      e.preventDefault();
+
+      const item={
+        email:this.state.email,
+        password:this.state.password
+      }
+
+      console.log(item);
+
+      axios.post("http://localhost:5000/users/find",item)
+        .then(response=>{
+          console.log(response.data);
+          if(response.data==="Email does not exist")
+          {
+            swal("Email not found! Please Register").then(()=>window.location.href="/register");
+          }
+          else if(response.data==="Password does not match")
+          {
+            swal("Password does not match");
+          }
+          else
+          {
+            swal("Successfully logged in").then(()=>window.location.href="/");
+            window.localStorage.setItem("_id",response.data._id);
+          }
+        })
+  }
+
   render() {
     return (
       <div className="signIn">
@@ -16,12 +63,13 @@ class Login extends Component {
             Welcome Back
           </h1>
 
-          <form>
-            <input type="email" name="email" placeholder="email" required />
+          <form onSubmit={this.loginUser}>
+            <input type="email" name="email" placeholder="email" onChange={this.handleChangeEmail} required />
             <input
               type="password"
               name="password"
               placeholder="password"
+              onChange={this.handleChangePassword}
               required
             />
 
