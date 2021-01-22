49891b66683cc5701144af8a9c5ff8278991934d
pranjaljain584 <62601082+pranjaljain584@users.noreply.github.com>
2021-01-22 21:33:42 +0530

    Routing and 404 page

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,27 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import "../css/Home.css";
-// import Navigationbar from "./Navigationbar";
+import HowItWorks from "./HowItWorks";
+import ContactUs from "./ContactUs";
 import backgroundImg from "../images/background.svg";
 
 export default class Home extends Component {
-    render() {
-        return (
-            <section style={{minHeight: "100vh"}}>
-             {/* <Navigationbar/> */}
-             <div className="Home">
-            <div className="Home-svg">
-                <img src={backgroundImg} alt=""/>
-            </div>
-            <div className="Home-desc">
-                <h1 className="Home-title"> <span className="student">Students</span> <br/> Grievance Redressal</h1>
-                <p className="Home-info">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+  render() {
+    return (
+      <section style={{ minHeight: "100vh" }}>
+        <div className="Home">
+          <div className="Home-svg">
+            <img src={backgroundImg} alt="" />
+          </div>
+          <div className="Home-desc">
+            <h1 className="Home-title">
+              {" "}
+              <span className="student">Students</span> <br /> Grievance
+              Redressal
+            </h1>
+            <p className="Home-info">
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </div>
+        </div>
 
-            </div>
-                
-            </div>
-                
-            </section>
-
-        )
-    }
+        <HowItWorks />
+        <ContactUs />
+      </section>
+    );
+  }
 }
