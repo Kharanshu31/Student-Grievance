7bfe2fe89d1c6dc70565f0aa073e34b06c546a15
pranjaljain584 <62601082+pranjaljain584@users.noreply.github.com>
2021-01-26 13:32:29 +0530

    create store

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -6,19 +6,25 @@ import React from "react";
 import Navigationbar from "./Navigationbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+// redux
+import { Provider } from "react-redux";
+import store from '../store';
+
 function App() {
   return (
-    <Router>
-      <div>
-        <Navigationbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route component={Page404} />
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Navigationbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route component={Page404} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
