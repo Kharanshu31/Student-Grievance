818c3ec50bb0a8c3a9e4e8c1ae0d85695af4816d
pranjaljain584 <62601082+pranjaljain584@users.noreply.github.com>
2021-01-28 15:48:37 +0530

    private routing

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -2,43 +2,79 @@ import Home from "./Home";
 import Login from "./Login";
 import Register from "./Register";
 import Page404 from "./Page404";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Navigationbar from "./Navigationbar";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import ResponsiveDrawer from "./ResponsiveDrawer";
 // redux
 import { connect } from "react-redux";
 import setAuthToken from "../utils/setAuthToken";
 import { loadUser } from "../actions/auth";
-import Form from './Form';
+import Form from "./Form";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
 }
 
-function App(props) {
+const PrivateRoute = (privateRouteProps) => {
+  console.log("privateRouteProps" , privateRouteProps) ;
+  const { isAuthenticated, component: Component, path } = privateRouteProps;
 
+  const [isLoggedIn , setLogIn] = useState(false) ;
+
+  // useEffect(() => {
+  //   if(isAuthenticated){
+
+  //   }
+  // })
+
+  return (
+    <Route
+      path={path}
+      render={(props) => {
+        return isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        );
+      }}
+    />
+  );
+};
+
+function App(props) {
   // component did mount
   useEffect(() => {
     props.dispatch(loadUser());
   }, []);
 
+  const { isAuthenticated } = props.auth;
+  console.log(isAuthenticated) ;
   return (
-    
-      <Router>
-        <div>
-          <Navigationbar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/dashboard" component={ResponsiveDrawer} />
-            <Route path="/submit" component={Form} />
-            <Route component={Page404} />
-          </Switch>
-        </div>
-      </Router>
-    
+    <Router>
+      <div>
+        <Navigationbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/dashboard" component={ResponsiveDrawer} />
+          {/* <Route path="/submit" component={Form} /> */}
+          <PrivateRoute
+            path="/submit"
+            component={Form}
+            isAuthenticated={props.auth.isAuthenticated}
+          />
+          <Route component={Page404} />
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/Components/TopBarAndDrawer.js
+++ frontend/src/Components/TopBarAndDrawer.js
@@ -1,105 +1,106 @@
-import React,{useState,useEffect} from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import IconButton from '@material-ui/core/IconButton';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
-import ErrorIcon from '@material-ui/icons/Error';
-import HomeIcon from '@material-ui/icons/Home';
-import InsertChartIcon from '@material-ui/icons/InsertChart';
-import MailIcon from '@material-ui/icons/Mail';
-import MenuIcon from '@material-ui/icons/Menu';
-import Button from '@material-ui/core/Button';
-import useStyles from './ResponiveDrawerStyles';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import AssignmentTurnedInIcon from "@material-ui/icons/AssignmentTurnedIn";
+import ErrorIcon from "@material-ui/icons/Error";
+import HomeIcon from "@material-ui/icons/Home";
+import InsertChartIcon from "@material-ui/icons/InsertChart";
+import MailIcon from "@material-ui/icons/Mail";
+import MenuIcon from "@material-ui/icons/Menu";
+import Button from "@material-ui/core/Button";
+import useStyles from "./ResponiveDrawerStyles";
 import { logout } from "../actions/auth";
-import {Redirect,withRouter} from 'react-router-dom' ;
+import { Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import {getcomplaint} from "../actions/complaint";
-
+import { getcomplaint } from "../actions/complaint";
 
 function TopBarAndDrawer(props) {
-    const { window } = props;
+  const { window } = props;
   const classes = useStyles();
   const theme = useTheme();
 
   const [mobileOpen, setMobileOpen] = useState(false);
   const [username, setUsername] = useState("Unknown");
-  const [loading,setloading]=useState(true);
-  const [pending,setPending]=useState(0);
-  const [completed,setCompleted]=useState(0);
+  const [loading, setloading] = useState(true);
+  const [pending, setPending] = useState(0);
+  const [completed, setCompleted] = useState(0);
   // const [complaintsLength,setComplaintLength]=useState(0);
-
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
 
-  const handleLogOut=()=>{
+  const handleLogOut = () => {
     props.dispatch(logout());
   };
 
   useEffect(() => {
     props.dispatch(getcomplaint());
 
-    props.complaint.complaints.map(el=>{
-        //console.log(el);
-        if(el.status)
-        {
-          setCompleted(prevstate=>prevstate+1)
-        }
-        else
-        {
-          setPending(prevstate=>prevstate+1)
-        }
-        //setComplaintLength(props.complaint.complaints.length)
-    })
-
-  },[]);
+    props.complaint.complaints.map((el) => {
+      //console.log(el);
+      if (el.status) {
+        setCompleted((prevstate) => prevstate + 1);
+      } else {
+        setPending((prevstate) => prevstate + 1);
+      }
+      //setComplaintLength(props.complaint.complaints.length)
+    });
+  }, []);
 
   useEffect(() => {
     // console.log('running');
-    if(props.auth.user && props.auth.token)
-    {
+    if (props.auth.user && props.auth.token) {
       console.log(props.auth);
-      setUsername(props.auth.user.name)
+      setUsername(props.auth.user.name);
       setloading(false);
     }
-  },[props.auth.user]);
-
+  }, [props.auth.user]);
 
   // useEffect(()=>{
   //       console.log(props.complaint.complaints);
   // },[props.complaint.complaints])
 
-  if(!props.auth.isAuthenticated)
-  {
-    return <Redirect to="/login" />
+  if (!props.auth.isAuthenticated) {
+    return <Redirect to="/login" />;
   }
 
-
-    const drawer = (
+  const drawer = (
     <div>
-
       <div className={classes.toolbar} />
       <Divider />
-      <h1 style={{marginLeft: "20px", fontSize: "36px", fontWeight:"200"}}>{username}</h1>
-      <h2 style={{marginLeft: "20px", fontSize: "18px", fontWeight:"bold"}}>User</h2>
+      <h1 style={{ marginLeft: "20px", fontSize: "36px", fontWeight: "200" }}>
+        {username}
+      </h1>
+      <h2 style={{ marginLeft: "20px", fontSize: "18px", fontWeight: "bold" }}>
+        User
+      </h2>
       <Divider />
       <List>
-        {['Dashboard', 'Submit New Complaint', 'Report'].map((text, index) => (
+        {["Dashboard", "Submit New Complaint", "Report"].map((text, index) => (
           <ListItem button key={text}>
-            <ListItemIcon>{index === 0 ? <HomeIcon/> : index === 1 ? <MailIcon/> : <InsertChartIcon/>}</ListItemIcon>
+            <ListItemIcon>
+              {index === 0 ? (
+                <HomeIcon />
+              ) : index === 1 ? (
+                <MailIcon />
+              ) : (
+                <InsertChartIcon />
+              )}
+            </ListItemIcon>
             <ListItemText primary={text} />
           </ListItem>
         ))}
@@ -113,34 +114,37 @@ function TopBarAndDrawer(props) {
           </ListItem>
         ))} */}
 
-        <ListItem key='Completed'>
-          <ListItemIcon><AssignmentTurnedInIcon style={{color:"green"}}/></ListItemIcon>
-          <ListItemText primary={`Completed : ${completed}`}/>
+        <ListItem key="Completed">
+          <ListItemIcon>
+            <AssignmentTurnedInIcon style={{ color: "green" }} />
+          </ListItemIcon>
+          <ListItemText primary={`Completed : ${completed}`} />
         </ListItem>
 
-        <ListItem key='Pending'>
-          <ListItemIcon><ErrorIcon style={{color:"red"}} /></ListItemIcon>
-          <ListItemText primary={`Pending : ${pending}`}/>
+        <ListItem key="Pending">
+          <ListItemIcon>
+            <ErrorIcon style={{ color: "red" }} />
+          </ListItemIcon>
+          <ListItemText primary={`Pending : ${pending}`} />
         </ListItem>
-
       </List>
     </div>
   );
 
-
-  const container = window !== undefined ? () => window().document.body : undefined;
-    return (
-        <>
-        <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar} >
-        <Toolbar style={
-            {
-                backgroundColor:'black',
-                display:'flex',
-                justifyContent:'space-between'
-
-            }
-        }>
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+  console.log("auth", props.auth.isAuthenticated);
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar
+          style={{
+            backgroundColor: "black",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -154,28 +158,24 @@ function TopBarAndDrawer(props) {
             Grievance Portal
           </Typography>
 
-
-
           <Button
-          onClick={handleLogOut}
-        variant="contained"
-        color="secondary"
-        className={classes.button}
-        endIcon={<ExitToAppIcon />}
-      >
-        Sign Out
-      </Button>
+            onClick={handleLogOut}
+            variant="contained"
+            color="secondary"
+            className={classes.button}
+            endIcon={<ExitToAppIcon />}
+          >
+            Sign Out
+          </Button>
         </Toolbar>
+      </AppBar>
 
-    </AppBar>
-
-      <nav className={classes.drawer} aria-label="mailbox folders" >
+      <nav className={classes.drawer} aria-label="mailbox folders">
         <Hidden smUp implementation="css">
           <Drawer
-
             container={container}
             variant="temporary"
-            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            anchor={theme.direction === "rtl" ? "right" : "left"}
             open={mobileOpen}
             onClose={handleDrawerToggle}
             classes={{
@@ -200,20 +200,19 @@ function TopBarAndDrawer(props) {
           </Drawer>
         </Hidden>
       </nav>
-
-        </>
-    )
+    </>
+  );
 }
 
 TopBarAndDrawer.propTypes = {
-   auth:PropTypes.object,
-  window: PropTypes.func
+  auth: PropTypes.object,
+  window: PropTypes.func,
 };
 
 function mapStateToProps(state) {
   return {
     auth: state.auth,
-    complaint:state.complaint
+    complaint: state.complaint,
   };
 }
 
